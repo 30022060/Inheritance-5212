7351d06671f43f78d60a3125d6bd9631125b7d6d
Liam Sullivan <30022060@student.toiohomai.ac.nz>
2019-08-08 10:36:58 +1200

    yes

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person FullName = new Person();
+            Console.Write("Enter First Name: ");
+            FullName.Fname = Console.ReadLine();
+            Console.Write("Enter Last Name: ");
+            FullName.Lname = Console.ReadLine();
+
+    }   }
+
+    class Person
+    {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+
+    }
+
+    class Student : Person
+    {
+        public int Studentid { get; set; }
+
+        public Student(int _studentid, string _fname, string _lname)
+        {
+            Fname = _fname;
+            Lname = _lname;
+            Studentid = _studentid;
+        }
+        public string StdDeets()
+        {
+            return $"{Fname}\n{Lname}\n{Studentid}";
+        }
+    }
+    
+}
